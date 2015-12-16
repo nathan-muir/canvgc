4ae0b4cff534cac8d09bc72961af507d7d35d87d
Nathan Muir <ndmuir@gmail.com>
2015-12-16 18:19:16 +1000

    Tweak example generation script to use absolute paths.

--- example/generate.sh
+++ example/generate.sh
@@ -1,2 +1,7 @@
-./../bin/canvgc simple.svg simple.js --prepend 'if(typeof window.canvgc=="undefined")window.canvgc={};window.canvgc.simple =' --append ';'
-./../bin/canvgc complex.svg complex.js --prepend 'if(typeof window.canvgc=="undefined")window.canvgc={};window.canvgc.complex =' --append ';'
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+PROJECT_ROOT=$( cd "$( dirname "$SCIRPT_DIR" )" && pwd )
+
+node "$PROJECT_ROOT/bin/canvgc" "$PROJECT_ROOT/example/simple.svg" "$PROJECT_ROOT/example/simple.js" --prepend 'if(typeof window.canvgc=="undefined")window.canvgc={};window.canvgc.simple =' --append ';'
+node "$PROJECT_ROOT/bin/canvgc" "$PROJECT_ROOT/example/complex.svg" "$PROJECT_ROOT/example/complex.js" --prepend 'if(typeof window.canvgc=="undefined")window.canvgc={};window.canvgc.complex =' --append ';'
